16a06feef4797771e385366f793f5b3f2f3c2460
tsuzuki-s <kiraboshi01@kiraboshi.page>
2021-10-28 09:14:13 +0900

    object型学習

---
+++ src/object.ts
@@ -0,0 +1,15 @@
+export {};
+
+let profile1: object = {
+  name: "Name",
+};
+
+profile1 = { birthYear: 1976 };
+
+let profile2: {
+  name: string;
+} = {
+  name: "Name",
+};
+
+profile2 = { name: "Naaaaame" };
